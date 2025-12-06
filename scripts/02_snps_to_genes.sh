9051810cd2a4638949cc0c5051001ab90d5049c0
ermia1313 <106460823+ermia1313@users.noreply.github.com>
2025-12-06 16:59:41 -0500

    Update 02_snps_to_genes.sh

--- scripts/02_snps_to_genes.sh
+++ scripts/02_snps_to_genes.sh
@@ -1,1 +1,18 @@
+# Running kirk.exe and spock.exe with work.txt
+
+# Combining the snps_list_hugo.txt.* into list_hugo_CAUSE.txt:
+
+echo "hugo wELPD wELPD^2 oELPD r2 tagged_SNP tag_SNP" \
+  | tr ' ' $'\t' > ./list_hugo_CAUSE.txt
+
+cat snps_list_hugo.txt.* \
+  | sort -t $'\t' -k 2,2 -g \
+  >> ./list_hugo_CAUSE.txt
+
+# Any checks like grep -H -w DCC & CAMKV.
+
+# Also move existing snps_to_genes.bat into this folder:
+
+./snps_to_genes.bat
+
 
