14c0096d103102b6b3f1e1980ed0aedb74ac5fe7
ermia1313 <106460823+ermia1313@users.noreply.github.com>
2025-12-06 17:03:42 -0500

    Update 03_prepare_pway_jobs.sh

--- scripts/03_prepare_pway_jobs.sh
+++ scripts/03_prepare_pway_jobs.sh
@@ -1,1 +1,12 @@
+# The loop
 
+for pway in DPs DrugBank GOBP GOCC GOMF Hallmark HumanCyc IOB KEGG MSigDB NCI Panther PathBank PFOCR Reactome TFs WikiPathways; do
+  nb_pways=`cat ./data/reference/Human_${pway}_fgsea.gmt | wc -l`
+  for ipway in `seq 1 ${nb_pways}`; do
+    echo /home/.../pway_empirical_P.bat ${pway} ${ipway}
+  done
+done | shuf > ./work.txt
+
+# The kirk.exe + spock.exe calls for the pathway jobs.
+
+# Move pway_empirical_P.bat into scripts/ too
