819cc924af7a8b7485f2cae60ee97c80f4351d3e
ermia1313 <106460823+ermia1313@users.noreply.github.com>
2025-12-07 15:21:02 -0500

    Update combine_pway_tables.R

--- R/combine_pway_tables.R
+++ R/combine_pway_tables.R
@@ -1,1 +1,27 @@
+#!/usr/bin/env Rscript
 
+args <- commandArgs(trailingOnly = TRUE)
+pway <- args[1]
+
+file_in  <- file.path("data", "results", paste0("pway_2024jun_", pway, ".txt"))
+file_out <- file_in
+
+DF <- read.table(
+  file_in,
+  sep = "\t", header = TRUE, as.is = TRUE, stringsAsFactors = FALSE,
+  comment.char = "", quote = ""
+)
+
+if (nrow(DF) > 0) {
+  w <- which(DF[, "sum"] < 0)
+  DF[w, "padj"] <- p.adjust(DF[w, "pval"], method = "fdr")
+  w <- which(DF[, "sum"] > 0)
+  DF[w, "padj"] <- p.adjust(DF[w, "pval"], method = "fdr")
+  O <- order(DF[, "pval"], -abs(DF[, "sum"]))
+  DF <- DF[O, ]
+}
+
+write.table(
+  DF, file = file_out,
+  sep = "\t", row.names = FALSE, col.names = TRUE, quote = FALSE
+)
